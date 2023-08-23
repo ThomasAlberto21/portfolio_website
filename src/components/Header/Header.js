f5a41e635c2cf5d59221f370d675d39bfe018601
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-23 10:43:57 +0700

    Header Finish

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,62 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Span,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from './HeaderStyles';
 
-const Header = () =>  (
+const Header = () => (
   <div>
-    Header
+    <Container>
+      <Div1>
+        <Link href='/'>
+          <a
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              color: 'white',
+              marginBottom: '20px',
+            }}
+          >
+            <DiCssdeck size='3rem' /> <Span>My Portfolio</Span>
+          </a>
+        </Link>
+      </Div1>
+      <Div2>
+        <li>
+          <Link href='#projects'>
+            <NavLink>Projects</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='#tech'>
+            <NavLink>Technologies</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='#about'>
+            <NavLink>About Me</NavLink>
+          </Link>
+        </li>
+      </Div2>
+      <Div3>
+        <SocialIcons href='https://github.com/ThomasAlberto21'>
+          <AiFillGithub size='3rem' />
+        </SocialIcons>
+        <SocialIcons href='https://www.instagram.com/thomasalberto21/'>
+          <AiFillInstagram size='3rem' />
+        </SocialIcons>
+        <SocialIcons href='www.linkedin.com/in/thomasalberto21'>
+          <AiFillLinkedin size='3rem' />
+        </SocialIcons>
+      </Div3>
+    </Container>
   </div>
 );
 
