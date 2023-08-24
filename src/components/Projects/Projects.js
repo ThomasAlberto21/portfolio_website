44f170089538c5f7cc7bfc8a69c41ceb2ccb0879
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-24 20:07:13 +0700

    .

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,50 +21,54 @@ import {
 } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
-const Projects = () => (
-  <Section id='projects'>
-    <SectionDivider />
-    <SectionTitle>Projects</SectionTitle>
-    <GridContainer>
-      {projects.map(
-        ({ id, image, title, description, tags, source, visit }) => (
-          <BlogCard key={id}>
-            <Img src={image} />
-            <TitleContent>
-              <HeaderThree title>{title}</HeaderThree>
-              <Hr />
-            </TitleContent>
-            <CardInfo>{description}</CardInfo>
-            <div>
-              <TitleContent>Stack</TitleContent>
-              <TagList>
-                {tags.map((tag, i) => (
-                  <Tag key={i}>{tag}</Tag>
-                ))}
-              </TagList>
-            </div>
-            <UtilityList>
-              <ExternalLinks href={visit}>Code</ExternalLinks>
-              <ExternalLinks href={source}>Source</ExternalLinks>
-            </UtilityList>
-          </BlogCard>
-        )
-      )}
-    </GridContainer>
-    <Button>
-      <Link href='/projects'>
-        <a
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            color: 'white',
-          }}
-        >
-          <span>See All Project</span>
-        </a>
-      </Link>
-    </Button>
-  </Section>
-);
+const Projects = () => {
+  const limitedProject = projects.slice(0, 4);
+
+  return (
+    <Section id='projects'>
+      <SectionDivider />
+      <SectionTitle>Projects</SectionTitle>
+      <GridContainer>
+        {limitedProject.map(
+          ({ id, image, title, description, tags, source, visit }) => (
+            <BlogCard key={id}>
+              <Img src={image} />
+              <TitleContent>
+                <HeaderThree title>{title}</HeaderThree>
+                <Hr />
+              </TitleContent>
+              <CardInfo>{description}</CardInfo>
+              <div>
+                <TitleContent>Stack</TitleContent>
+                <TagList>
+                  {tags.map((tag, i) => (
+                    <Tag key={i}>{tag}</Tag>
+                  ))}
+                </TagList>
+              </div>
+              <UtilityList>
+                <ExternalLinks href={visit}>Code</ExternalLinks>
+                <ExternalLinks href={source}>Source</ExternalLinks>
+              </UtilityList>
+            </BlogCard>
+          )
+        )}
+      </GridContainer>
+      <Button>
+        <Link href='/projects'>
+          <a
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              color: 'white',
+            }}
+          >
+            <span>See All Project</span>
+          </a>
+        </Link>
+      </Button>
+    </Section>
+  );
+};
 
 export default Projects;
